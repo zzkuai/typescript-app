5dc7023d1c55e409c45fa281b92850b1b4fac848
zzkuai <zzkuai@gmail.com>
2021-06-13 22:43:37 +0800

    feat: 添加登录逻辑

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,22 @@
-import express, { Request, Response, NextFunction } from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
+import cookieSession from 'cookie-session'
 import router from './router'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['zzkuai'],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours
+  })
+)
+
 app.use(router)
 
 app.listen(3000, () => {
--- src/router.ts
+++ src/router.ts
@@ -14,25 +14,72 @@ interface RequestWithBody extends Request {
 const router = Router()
 
 router.get('/', (req: RequestWithBody, res: Response) => {
-  const html = fs.readFileSync(
-    path.resolve(__dirname, '../static/login.html'),
-    'utf-8'
-  )
+  const isLogin = req.session?.login
+  let html
+
+  if (isLogin) {
+    html = fs.readFileSync(
+      path.resolve(__dirname, '../static/logout.html'),
+      'utf-8'
+    )
+  } else {
+    html = fs.readFileSync(
+      path.resolve(__dirname, '../static/login.html'),
+      'utf-8'
+    )
+  }
+
   res.writeHead(200, { 'Content-Type': 'text/html;charset=UTF-8' })
   res.end(html)
 })
 
-router.post('/getData', (req: RequestWithBody, res: Response) => {
-  const { password } = req.body
+router.get('/logout', (req: Request, res: Response) => {
+  if (req.session) {
+    req.session.login = undefined
+  }
 
-  if (password !== '123') return res.send('password error')
+  res.redirect('/')
+})
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { password } = req.body
+  const isLogin = req.session?.login
+
+  if (isLogin) {
+    res.send('历史用户 登录成功')
+  } else {
+    if (password !== '123') return res.send('登录失败')
+
+    if (req.session) {
+      req.session.login = true
+      res.send('登录成功')
+    }
+  }
+})
+
+router.get('/getData', (req: Request, res: Response) => {
+  const isLogin = req.session?.login
+
+  if (!isLogin) return res.send('请先登录')
 
   const secret = 'x3b174jsx'
   const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
   const analyzer = CourseAnalyzer.getInstance()
   new Crawler(url, analyzer)
 
-  res.send('get data success')
+  res.send('数据爬取成功')
+})
+
+router.get('/showData', (req: Request, res: Response) => {
+  const isLogin = req.session?.login
+  if (!isLogin) return res.send('请先登录')
+  try {
+    const position = path.resolve(__dirname, '../data/course.json')
+    const result = fs.readFileSync(position, 'utf-8')
+    res.json(JSON.parse(result))
+  } catch (e) {
+    res.send('尚未爬取到数据')
+  }
 })
 
 export default router
