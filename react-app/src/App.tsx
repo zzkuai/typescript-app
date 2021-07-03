45e6e553e1970429ccba0d3b09d42be86e7241c2
zzkuai <zzkuai@gmail.com>
2021-07-03 09:26:00 +0800

    feat: 完成登录页面样式

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import Login from './Login/Login'
 
 function App() {
-  return <div className="App">hello world</div>
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  )
 }
 
 export default App
